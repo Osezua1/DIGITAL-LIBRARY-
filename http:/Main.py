eda56e0a19989be3d938ade37e5de97f7cf7c3f4
Osezua1 <140377320+Osezua1@users.noreply.github.com>
2023-07-27 17:54:00 +0100

    Update Main.py

--- http:/Main.py
+++ http:/Main.py
@@ -1,5 +1,3 @@
-import tkinter as tk
-from tkinter import messagebox
 import sqlite3
 from getpass import getpass
 from datetime import datetime
@@ -42,111 +40,84 @@ conn.commit()
 
 # Function to add a user to the database
 def register_user():
-    username = entry_username.get()
-    password = entry_password.get()
+    username = input("Enter a username: ")
+    password = getpass("Enter a password: ")
 
     if username and password:
         cursor.execute('INSERT INTO users (username, password) VALUES (?, ?)', (username, password))
         conn.commit()
-        messagebox.showinfo("Success", "User registration successful.")
+        print("User registration successful.")
     else:
-        messagebox.showerror("Error", "Please fill in all the fields.")
+        print("Please fill in all the fields.")
 
 # Function to authenticate a user
 def login_user():
-    username = entry_username.get()
-    password = entry_password.get()
+    username = input("Enter your username: ")
+    password = getpass("Enter your password: ")
 
     if username and password:
         cursor.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username, password))
         user = cursor.fetchone()
 
         if user:
-            messagebox.showinfo("Success", "Login successful.")
+            print("Login successful.")
             return user[0]  # Return the user ID
         else:
-            messagebox.showerror("Error", "Invalid username or password.")
+            print("Invalid username or password.")
             return None
     else:
-        messagebox.showerror("Error", "Please fill in all the fields.")
+        print("Please fill in all the fields.")
         return None
 
 # Function to add a book to the database
 def add_book():
-    title = entry_title.get()
-    author = entry_author.get()
-    pub_date = entry_pub_date.get()
-    keywords = entry_keywords.get()
+    title = input("Enter the title of the book: ")
+    author = input("Enter the author of the book: ")
+    pub_date = input("Enter the publication date of the book: ")
+    keywords = input("Enter keywords for the book (comma-separated): ")
 
     if title and author and pub_date and keywords:
         cursor.execute('INSERT INTO books (title, author, publication_date, keywords) VALUES (?, ?, ?, ?)',
                        (title, author, pub_date, keywords))
         conn.commit()
-        messagebox.showinfo("Success", "Book added to the library.")
+        print("Book added to the library.")
     else:
-        messagebox.showerror("Error", "Please fill in all the fields.")
+        print("Please fill in all the fields.")
 
 # Function to search for books in the database
 def search_books():
-    search_term = entry_search.get()
+    search_term = input("Enter a search term (title, author, or keyword): ")
 
     if search_term:
         cursor.execute('SELECT * FROM books WHERE title LIKE ? OR author LIKE ? OR keywords LIKE ?',
                        ('%'+search_term+'%', '%'+search_term+'%', '%'+search_term+'%'))
         books = cursor.fetchall()
-        display_search_results(books)
+        if books:
+            print("Search Results:")
+            for book in books:
+                print(f"{book[1]} by {book[2]} ({book[3]}) - Keywords: {book[4]}")
+        else:
+            print("No books found.")
     else:
-        messagebox.showerror("Error", "Please enter a search term.")
-
-# Function to display search results in a new window
-def display_search_results(books):
-    results_window = tk.Toplevel(root)
-    results_window.title("Search Results")
-    results_window.geometry("600x400")
-
-    result_listbox = tk.Listbox(results_window, width=70, height=20)
-    result_listbox.pack()
-
-    for book in books:
-        result_listbox.insert(tk.END, f"{book[1]} by {book[2]} ({book[3]}) - Keywords: {book[4]}")
+        print("Please enter a search term.")
 
 # Function to view book details
 def view_book_details():
-    book_id = entry_book_id.get()
+    book_id = input("Enter the book ID to view details: ")
     cursor.execute('SELECT * FROM books WHERE id = ?', (book_id,))
     book = cursor.fetchone()
 
     if book:
-        details_window = tk.Toplevel(root)
-        details_window.title("Book Details")
-        details_window.geometry("400x200")
-
-        label_title = tk.Label(details_window, text="Title:")
-        label_title.pack()
-        label_title_value = tk.Label(details_window, text=book[1])
-        label_title_value.pack()
-
-        label_author = tk.Label(details_window, text="Author:")
-        label_author.pack()
-        label_author_value = tk.Label(details_window, text=book[2])
-        label_author_value.pack()
-
-        label_pub_date = tk.Label(details_window, text="Publication Date:")
-        label_pub_date.pack()
-        label_pub_date_value = tk.Label(details_window, text=book[3])
-        label_pub_date_value.pack()
-
-        label_keywords = tk.Label(details_window, text="Keywords:")
-        label_keywords.pack()
-        label_keywords_value = tk.Label(details_window, text=book[4])
-        label_keywords_value.pack()
-
+        print("Title:", book[1])
+        print("Author:", book[2])
+        print("Publication Date:", book[3])
+        print("Keywords:", book[4])
     else:
-        messagebox.showerror("Error", "Book not found.")
+        print("Book not found.")
 
 # Function for book borrowing
 def borrow_book(user_id):
-    book_id = entry_borrow.get()
+    book_id = input("Enter the book ID to borrow: ")
 
     # Check if the book is available
     cursor.execute('SELECT * FROM books WHERE id = ?', (book_id,))
@@ -156,13 +127,13 @@ def borrow_book(user_id):
         cursor.execute('INSERT INTO book_loans (user_id, book_id, borrow_date) VALUES (?, ?, ?)',
                        (user_id, book_id, datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
         conn.commit()
-        messagebox.showinfo("Success", "Book borrowed successfully.")
+        print("Book borrowed successfully.")
     else:
-        messagebox.showerror("Error", "Book not found.")
+        print("Book not found.")
 
 # Function for book returning
 def return_book(user_id):
-    book_id = entry_return.get()
+    book_id = input("Enter the book ID to return: ")
 
     # Check if the book is on loan by the user
     cursor.execute('SELECT * FROM book_loans WHERE user_id = ? AND book_id = ? AND return_date IS NULL',
@@ -173,37 +144,46 @@ def return_book(user_id):
         cursor.execute('UPDATE book_loans SET return_date = ? WHERE id = ?',
                        (datetime.now().strftime('%Y-%m-%d %H:%M:%S'), loan[0]))
         conn.commit()
-        messagebox.showinfo("Success", "Book returned successfully.")
+        print("Book returned successfully.")
     else:
-        messagebox.showerror("Error", "You haven't borrowed this book or it has already been returned.")
+        print("You haven't borrowed this book or it has already been returned.")
 
 # Main program loop
 def main():
     user_id = None
 
-    root = tk.Tk()
-    root.title("Digital Library")
-    root.geometry("400x600")
+    while True:
+        print("\n1. Register\n2. Login\n3. Add Book\n4. Search Books\n5. View Book Details")
+        print("6. Borrow Book\n7. Return Book\n8. Exit")
+        choice = input("Enter your choice: ")
 
-    # User Authentication GUI
-    label_username = tk.Label(root, text="Username:")
-    label_username.pack()
-    entry_username = tk.Entry(root)
-    entry_username.pack()
+        if choice == "1":
+            register_user()
+        elif choice == "2":
+            user_id = login_user()
+        elif choice == "3":
+            add_book()
+        elif choice == "4":
+            search_books()
+        elif choice == "5":
+            view_book_details()
+        elif choice == "6":
+            if user_id:
+                borrow_book(user_id)
+            else:
+                print("Please log in to borrow a book.")
+        elif choice == "7":
+            if user_id:
+                return_book(user_id)
+            else:
+                print("Please log in to return a book.")
+        elif choice == "8":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
 
-    label_password = tk.Label(root, text="Password:")
-    label_password.pack()
-    entry_password = tk.Entry(root, show="*")
-    entry_password.pack()
+    conn.close()
 
-    btn_register = tk.Button(root, text="Register", command=register_user)
-    btn_register.pack()
-
-    btn_login = tk.Button(root, text="Login", command=lambda: login_user())
-    btn_login.pack()
-
-    # Book Management GUI
-    label_title = tk.Label(root, text="Title:")
-    label_title.pack()
-    entry_title = tk.Entry(root)
-    entry_title
+if __name__ == "__main__":
+    main()
